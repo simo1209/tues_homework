d232f37723d4c62fd638f55295c1d2b8348bb746
SkyDoge69 <luc2002penta@gmail.com>
2017-03-12 00:05:55 +0200

    Add files via upload

---
+++ 1/for_traverse.py
@@ -0,0 +1,23 @@
+Python 2.7.13 (v2.7.13:a06454b1afa1, Dec 17 2016, 20:42:59) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> def for_traverse():
+	books = ["Learn You a Haskell",
+        "The Healthy Programmer",
+        "Code Complete",
+        "The Pragmatic Programmer",
+        "Pro Git",
+        "Introduction to Algorithms",
+        "Concrete Mathematics"]
+	for book in books:
+		print(books)
+
+		
+>>> for_traverse()
+['Learn You a Haskell', 'The Healthy Programmer', 'Code Complete', 'The Pragmatic Programmer', 'Pro Git', 'Introduction to Algorithms', 'Concrete Mathematics']
+['Learn You a Haskell', 'The Healthy Programmer', 'Code Complete', 'The Pragmatic Programmer', 'Pro Git', 'Introduction to Algorithms', 'Concrete Mathematics']
+['Learn You a Haskell', 'The Healthy Programmer', 'Code Complete', 'The Pragmatic Programmer', 'Pro Git', 'Introduction to Algorithms', 'Concrete Mathematics']
+['Learn You a Haskell', 'The Healthy Programmer', 'Code Complete', 'The Pragmatic Programmer', 'Pro Git', 'Introduction to Algorithms', 'Concrete Mathematics']
+['Learn You a Haskell', 'The Healthy Programmer', 'Code Complete', 'The Pragmatic Programmer', 'Pro Git', 'Introduction to Algorithms', 'Concrete Mathematics']
+['Learn You a Haskell', 'The Healthy Programmer', 'Code Complete', 'The Pragmatic Programmer', 'Pro Git', 'Introduction to Algorithms', 'Concrete Mathematics']
+['Learn You a Haskell', 'The Healthy Programmer', 'Code Complete', 'The Pragmatic Programmer', 'Pro Git', 'Introduction to Algorithms', 'Concrete Mathematics']
+>>> 
